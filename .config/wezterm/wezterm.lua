6b9223d1831def5c0313101321cfb3b81614410b
Cory Smith <smith.cory@gmail.com>
2025-08-10 10:35:57 -0700

    added F keys for aws/kubernetes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,28 @@ local config = {}
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 
+config.keys = {
+	{
+		key = "F2",
+		mods = "NONE",
+		action = wezterm.action.SendString("k_fzf_pod\n"),
+	},
+	{
+		key = "F2",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("k_fzf_ns\n"),
+	},
+	{
+		key = "F12",
+		mods = "NONE",
+		action = wezterm.action.SendString("aws_fzf\n"),
+	},
+	{
+		key = "F12",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("aws_fzf_ec2\n"),
+	},
+}
 return config
